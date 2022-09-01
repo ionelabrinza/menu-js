efe58d41036ad6c2ba2bfcef239e4eec830a82ec
ionelabrinza <ionela.brinza@yahoo.com>
2022-09-01 15:47:41 -0500

    refactoring

--- app.js
+++ app.js
@@ -76,7 +76,11 @@ const menu = [{
 const sectionCenter = document.querySelector('.section-center');
 
 window.addEventListener('DOMContentLoaded', function() {
-    let displayMenu = menu.map(function(item) {
+    displayMenuItems(menu)
+});
+
+function displayMenuItems(menuItems) {
+    let displayMenu = menuItems.map(function(item) {
         return `<article class="menu-item">
       <img src=${item.img} class="photo" alt=${item.title}>
       <div class="item-info">
@@ -84,10 +88,10 @@ window.addEventListener('DOMContentLoaded', function() {
               <h4>${item.title}</h4>
               <h4 class="price">${item.price}</h4>
           </header>
-          <p class="item-text">${item.desc}</p>
+          <p class="item-text">$${item.desc}</p>
       </div>
   </article>`;
     });
     displayMenu = displayMenu.join('');
     sectionCenter.innerHTML = displayMenu
-});
+}

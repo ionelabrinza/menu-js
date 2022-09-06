0d31df91c19e55f31906046b3ab82bdc6218edef
ionelabrinza <ionela.brinza@yahoo.com>
2022-09-05 21:06:52 -0500

    filter items

--- app.js
+++ app.js
@@ -74,10 +74,34 @@ const menu = [{
 ];
 
 const sectionCenter = document.querySelector('.section-center');
+const filterBtns = document.querySelectorAll('.filter-btn')
+
+//load items
 
 window.addEventListener('DOMContentLoaded', function() {
     displayMenuItems(menu)
 });
+
+// filter items
+
+filterBtns.forEach(function(btn) {
+    btn.addEventListener('click', function(e) {
+        const category = e.currentTarget.dataset.id;
+        const menuCategory = menu.filter(function(menuItem) {
+            // console.log(menuItem.category);
+            if (menuItem.category === category) {
+                return menuItem;
+            }
+        });
+        //console.log(menuCategory)
+        if (category === 'all') {
+            displayMenuItems(menu)
+        } else {
+            displayMenuItems(menuCategory);
+        }
+    });
+});
+
 
 function displayMenuItems(menuItems) {
     let displayMenu = menuItems.map(function(item) {
